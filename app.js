39690de0327bb6572a047cb5c155df3cb07b9473
Nils Blaumer <blaumer@gemedico.com>
2019-08-13 01:53:33 +0200

    remove unused code

--- app.js
+++ app.js
@@ -12,32 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Routes
 app.use('/api', router);
 
-// app.post('/api/v1/todos', (req, res, next) => {
-//     if(!req.body.title) {
-//         return res.status(400).send({
-//             success: 'false',
-//             message: 'title is required',
-//         });
-//     }
-//     else if(!req.body.description) {
-//         return res.status(400).send({
-//             success: 'false',
-//             message: 'description is required',
-//         });
-//     }
-//     const todo = {
-//         id: todos.length + 1,
-//         title: req.body.title,
-//         description: req.body.description,
-//     }
-//     todos.push(todo);
-//     return res.status(201).send({
-//         success: 'true',
-//         message: 'todo added successfully',
-//         todo,
-//     });
-// });
-
 // Start Server
 app.listen(3000, () => {
     console.log('Server started');
